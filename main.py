0b428be193640fc0b036ce2d88991626a23d189b
youshitsune <luka@youshitsune.me>
2023-04-04 18:45:11 +0200

    Implement client

--- main.py
+++ main.py
@@ -167,5 +167,46 @@ class Server(object):
         for key, value in data:
             self._kv[key] = value
         return len(data)
+
     def run(self):
         self._server.serve_forever()
+
+
+class Client(object):
+    def __init__(self, host='127.0.0.1', port=31337):
+        self._protocol = ProtocolHandler()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self._fh = self._socket.makefile('rwb')
+
+    def execute(self, *args):
+        self._protocol.write_responses(self._fh, args)
+        resp = self._protocol.handle_request(self._fh)
+        if isinstance(resp, Error):
+            raise CommandError(resp.message)
+        return resp
+
+    def get(self, key):
+        return self.execute('GET', key)
+
+    def set(self, key, value):
+        return self.execute('SET', key, value)
+
+    def delete(self, key):
+        return self.execute('DELETE', key)
+
+    def flush(self):
+        return self.execute('FLUSH')
+
+    def mget(self, *keys):
+        return self.execute('MGET', *keys)
+
+    def mset(self, *items):
+        return self.execute('MSET', *items)
+
+
+if __name__ == "__main__":
+    from gevent import monkey
+    monkey.patch_all()
+    Server().run()
+
