c96180d0d0f63d16c4ae84cd7ec696ff5c677db6
youshitsune <luka@youshitsune.me>
2023-04-04 18:39:32 +0200

    Implement commands

--- main.py
+++ main.py
@@ -99,6 +99,15 @@ class Server(object):
 
         self._protocol = ProtocolHandler()
         self._kv = {}
+    
+    def get_commands(self):
+        return {
+                'GET': self.get,
+                'SET': self.set,
+                'DELETE': self.delete,
+                'FLUSH': self.flush,
+                'MGET': self.mget,
+                'MSET': self.mset}
 
     def connection_handler(self, conn, address):
         socket_file = conn.makefile('rwb')
@@ -117,7 +126,46 @@ class Server(object):
             self._protcol.write_response(socket_file, resp)
 
     def get_response(self, data):
-        pass
+        if not isinstance(data, list):
+            try:
+                data = data.split()
+            except Exception:
+                raise CommandError('Request must be list or simple string.')
 
+        if not data:
+            raise CommandError('Missing command')
+
+        command = data[0].upper()
+        if command not in self._commands:
+            raise CommandError(f'Unrecognized command: {command}')
+
+        return self._commands[command](*data[1:])
+
+    def get(self, key):
+        return self._kv.get(key)
+
+    def set(self, key, value):
+        self._kv[key] = value
+        return 1
+    
+    def delete(self, key):
+        if key in self._kv:
+            del self._kv[key]
+            return 1
+        return 0
+
+    def flush(self):
+        kvlen = len(self._kv)
+        self._kv.clear()
+        return kvlen
+
+    def mget(self, *keys):
+        return [self._kv.get(key) for key in keys]
+
+    def mset(self, *items):
+        data = zip(items[::2], items[1::2])
+        for key, value in data:
+            self._kv[key] = value
+        return len(data)
     def run(self):
         self._server.serve_forever()
